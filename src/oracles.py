94f1e1f42485d8c505b4ac026c225d6dd9606360
ajlandow <ajlandow@ncsu.edu>
2020-11-11 21:07:45 -0500

    add diffuser

--- src/oracles.py
+++ src/oracles.py
@@ -24,12 +24,16 @@ def generate_oracles(s, input_string, pattern_length):
 
         N = len(input_string)
         M = pattern_length
-        for i in range( N - M + 1 ):
+        for i in range( 2**s ):
+            print(f'{input_string[i]} =? {pattern_char}')
             if input_string[i] == pattern_char:
                 pattern_char_oracle_matrix[i, i] = -1
 
+        # print(pattern_char_oracle_matrix)
+        # print()
         oracles[pattern_char] = quantum_info.Operator( pattern_char_oracle_matrix )
 
     wildcard_oracle_matrix = np.multiply( -1, np.identity( int( 2**s ) ) )
+    # print(wildcard_oracle_matrix)
     oracles['*'] = quantum_info.Operator( wildcard_oracle_matrix )
     return oracles
--- src/pattern_match.py
+++ src/pattern_match.py
@@ -28,16 +28,26 @@ References
     https://qiskit.org/documentation/stubs/qiskit.circuit.ControlledGate.html
     https://stackoverflow.com/questions/61286794/is-there-an-anti-control-gate-in-qiskit
     https://pynative.com/python-random-randrange/#:~:text=Use%20randint()%20when%20you,number%20from%20an%20exclusive%20range.
+    https://stackoverflow.com/questions/339007/how-to-pad-zeroes-to-a-string
+    https://stackoverflow.com/questions/40999973/how-to-pad-a-numeric-string-with-zeros-to-the-right-in-python
 
 '''
 
 import math
 
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
-from qiskit.circuit.library.standard_gates import XGate
 from qiskit import Aer, IBMQ, execute
+from qiskit import quantum_info
+
+from qiskit.circuit.library.standard_gates import XGate
+
 from oracles import generate_oracles
 from random import randint
+
+import numpy as np
+import pprint
+
+
 
 '''
 Example for s = 3:
@@ -54,6 +64,7 @@ qr_p1 {  -----------------------
 
 '''
 qubits_for_pattern_chars = dict()
+simulatorBackend = Aer.get_backend('qasm_simulator')
 
 # The control state is a list of 0's and 1's that encodes whether a 0 or a 1
 #   will activate the control for the i_th qubit in the control state string
@@ -107,38 +118,59 @@ def create_initial_state(qc, s, M):
     # foreach (M - 1): add_pattern_char_state()
     # sequentially entangle each character
     # each character represented by s qubits, s is number of bits in index
-    for i in range(1, M + 1):
+    for i in range(1, M):
         _add_pattern_char_state(qc, i, s)
+
+def diffuser(qc, s):
+    qc.h( qubits_for_pattern_chars[0] )
+
+    diffuser_matrix = np.identity( int( 2**s ) )
+    diffuser_matrix[0, 0] = -1
+    qubit_start_index = 0
+    qubit_stop_index  = qubit_start_index + s
+    qc.unitary( quantum_info.Operator( diffuser_matrix ), range( qubit_start_index, qubit_stop_index ) )
+
+    qc.h( qubits_for_pattern_chars[0] )
 
 
 def pattern_match(qc, oracles, pattern, s):
     # j: index of a char in the pattern
     # a. Choose j randomly from [1, M]
     j = randint( 0, len(pattern) - 1 )
+    print(f'j = {j}')
 
     # b. Apply Q_(p_j) to the set of s qubits that represent pattern char j
     qubit_start_index = j * s
     qubit_stop_index  = qubit_start_index + s
     pattern_char = pattern[j]
     qc.unitary( oracles[ pattern_char ], range( qubit_start_index, qubit_stop_index ) )
+    qc.barrier()
 
     # c. Apply Diffusion Operator to the entire state psi
+    diffuser(qc, s)
 
+    qc.barrier()
 
-    pass
+    return
 
 
 if __name__ == '__main__':
     # get input string
     # get pattern
-    input_string = '001000000'
+    input_string = '010'.zfill( 2**4 )
+    padded_length = 2**( math.ceil(math.log2(len(input_string))) )
+    input_string = input_string.ljust( padded_length, '0' )
+    print( f'input_string = {input_string}' )
     N = len(input_string)
 
-    pattern = '1*'
+    pattern = '01'
     M = len(pattern)
+    print( f'pattern = {pattern}' )
 
     # N - M + 1 = 2^s
     s = math.ceil( math.log2(N - M + 1) ) # round up if N - M + 1 is not a power of 2
+    print(f's = {s}')
+
 
     qc = QuantumCircuit()
     oracles = generate_oracles( s, input_string, len(pattern) )
@@ -147,11 +179,22 @@ if __name__ == '__main__':
 
     # run ~sqrt(N) times
     number_of_iterations = math.ceil( math.sqrt( math.pow(s, 2) ) )
+    print(f'number_of_iterations = {number_of_iterations}')
     for q in range(number_of_iterations):
         pattern_match( qc, oracles, pattern, s )
 
+    classicalRegisters = ClassicalRegister(s)
+    qc.add_register(classicalRegisters)
+    qc.measure( qubits_for_pattern_chars[0], classicalRegisters )
 
-    # qc.draw(output = 'mpl', plot_barriers = False, filename = "test1.png")
+    qc.draw(output = 'mpl', plot_barriers = True, filename = "test2.png")
     print( qc )
 
-    # get the oracle corresponding to each letter in the pattern
+    backend = simulatorBackend
+    # backend = IBMQBackend
+    shots = 2**13
+    job = execute(qc, backend = backend, shots = shots)
+    results = job.result()
+    counts = results.get_counts()
+
+    pprint.pprint(counts)
